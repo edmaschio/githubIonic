068d09ea6ae270ac7971bda663f641054cb01dc6
Ederson Maschio <edmaschio@gmail.com>
2016-12-16 20:02:01 -0200

    Searchbar and style color

--- src/providers/github-users.ts
+++ src/providers/github-users.ts
@@ -18,13 +18,20 @@ export class GithubUsers {
 
   constructor(public http: Http) {}
 
+  // Load all github users (limit = 50)
   load(): Observable<User[]> {
     return this.http.get(`${this.githubApiUrl}/users`)
       .map(res => <User[]>res.json());
   }
 
+  // Get github user by providing login(username)
   loadDetails(login: string): Observable<User> {
     return this.http.get(`${this.githubApiUrl}/users/${login}`)
               .map(res => <User>(res.json()))
+  }
+
+  searchUsers(searchParam: string): Observable<User[]> {
+    return this.http.get(`${this.githubApiUrl}/search/users?q=${searchParam}`)
+               .map(res => <User[]>(res.json().items))
   }
 }
